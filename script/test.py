663c882c56e25c04811703a04fe1cae8b1a1ba56
venom21adi <69112627+venom21adi@users.noreply.github.com>
2023-05-25 21:21:17 +0530

    Add files via upload

--- script/test.py
+++ script/test.py
@@ -3,6 +3,7 @@ accountName = "flaskdemoappstorage"
 accountKey = "prbvCQU0nU+u3/Svm8rL/+r3HZWUlSkzu7T5xHg8ihe3U08wxL+eQTC8PVIvf/AmQMHFbiJCufmA+AStoGmxGw=="
 containerName = "flaskdemoappcontainer"
 sas_Token ="?sv=2022-11-02&ss=bfqt&srt=sco&sp=rwdlacupiytfx&se=2023-05-25T23:34:36Z&st=2023-05-25T15:34:36Z&spr=https&sig=wFfO5qt6ZdMQWL5H5FxcVPtFSNBcr5p1jQrHfjdKi8g%3D"
+conn_str = "DefaultEndpointsProtocol=https;AccountName=flaskdemoappstorage;AccountKey=prbvCQU0nU+u3/Svm8rL/+r3HZWUlSkzu7T5xHg8ihe3U08wxL+eQTC8PVIvf/AmQMHFbiJCufmA+AStoGmxGw==;EndpointSuffix=core.windows.net"
 #blobName = "test3.json"
 import pandas as pd
 lst =[]
@@ -21,7 +22,7 @@ def test():
 
     output = df.to_csv("Test.csv")
 
-    blobService = BlockBlobService(account_name=accountName, account_key = accountKey,sas_token =sas_Token)
+    blobService = BlockBlobService(account_name=accountName, connection_string= conn_str)
 
     #blobService.create_blob_from_text(containerName, 'Test', output)
 
