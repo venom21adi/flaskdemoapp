53f49647cf3093f7c35e3165977dd66fae971ebf
venom21adi <69112627+venom21adi@users.noreply.github.com>
2023-05-25 19:06:14 +0530

    Add files via upload

--- script/test.py
+++ script/test.py
@@ -1,4 +1,4 @@
-#from azure.storage.blob import BlockBlobService
+from azure.storage.blob import *
 accountName = "flaskdemoappstorage"
 accountKey = "prbvCQU0nU+u3/Svm8rL/+r3HZWUlSkzu7T5xHg8ihe3U08wxL+eQTC8PVIvf/AmQMHFbiJCufmA+AStoGmxGw=="
 containerName = "flaskdemoappcontainer"
@@ -18,10 +18,10 @@ def test():
     
     df = pd.DataFrame(lst)
 
-    #output = df.to_csv("Test.csv")
+    output = df.to_csv("Test.csv")
 
-    #blobService = BlockBlobService(account_name=accountName, account_key=accountKey)
+    blobService = BlockBlobService(account_name=accountName, account_key=accountKey)
 
-    #blobService.create_blob_from_text(containerName, 'Test', output)
+    blobService.create_blob_from_text(containerName, 'Test', output)
 
     
