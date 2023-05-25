93e5c439b6137ccd47ff11708f9b14e4128a09eb
venom21adi <69112627+venom21adi@users.noreply.github.com>
2023-05-25 20:57:01 +0530

    Add files via upload

--- script/test.py
+++ script/test.py
@@ -2,7 +2,7 @@ from azure.storage.blob import *
 accountName = "flaskdemoappstorage"
 accountKey = "prbvCQU0nU+u3/Svm8rL/+r3HZWUlSkzu7T5xHg8ihe3U08wxL+eQTC8PVIvf/AmQMHFbiJCufmA+AStoGmxGw=="
 containerName = "flaskdemoappcontainer"
-sas_token ="sp=racwdli&st=2023-05-25T14:45:13Z&se=2023-05-25T22:45:13Z&spr=https&sv=2022-11-02&sr=c&sig=TJsqK%2FQ%2FzjW%2FmCPrJLMzDdR8diyNikbMdGyIl9cCnKw%3D"
+sas_Token ="sp=racwdli&st=2023-05-25T15:25:32Z&se=2023-05-25T23:25:32Z&spr=https&sv=2022-11-02&sr=c&sig=OSBDUVh8Pkg%2FHaZae%2BjIh0v5dphn1T3Eq42Mc3wusnk%3D"
 #blobName = "test3.json"
 import pandas as pd
 lst =[]
@@ -19,9 +19,9 @@ def test():
     
     df = pd.DataFrame(lst)
 
-    #output = df.to_csv("Test.csv")
+    output = df.to_csv("Test.csv")
 
-    blobService = BlockBlobService(account_name=accountName, account_key =accountKey)
+    blobService = BlockBlobService(account_name=accountName, sas_token =sas_Token)
 
     #blobService.create_blob_from_text(containerName, 'Test', output)
 
