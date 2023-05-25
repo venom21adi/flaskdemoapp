0a1b72daf0f09f75afc6bcadafb673b1f64c9f49
venom21adi <69112627+venom21adi@users.noreply.github.com>
2023-05-25 13:54:59 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -2,11 +2,15 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
+"""
 @app.route('/')
 def index():
     return render_template('index.html')
-    
+"""
+@app.route('/')
+def index():
+    return "Hello World!"
+   
 
 if __name__ == "__main__":
     app.secret_key = "ThisisNotASecret:p"
