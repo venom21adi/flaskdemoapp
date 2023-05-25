c09933ec86604884c51ec276d092409ce32e3613
venom21adi <69112627+venom21adi@users.noreply.github.com>
2023-05-25 16:00:30 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -6,6 +6,13 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+    
+    
+@app.route('/page2')
+def page2():
+    from script.test import test
+    x = test()
+    return render_template('page2.html')    
 """
 @app.route('/')
 def index():
