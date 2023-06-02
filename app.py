cf2086b5db9ee6f74a838e4c999808f6d7c1d87f
venom21adi <69112627+venom21adi@users.noreply.github.com>
2023-06-02 13:33:40 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,44 +1,40 @@
-from flask import *
+from flask import Flask, request, render_template, redirect, url_for
+from werkzeug.utils import secure_filename
+from azure.storage.blob import BlobServiceClient
 import os
-#from azure.storage.blob import BlobServiceClient
+
 app = Flask(__name__)
 
+app.config.from_pyfile('config.py')
+account = app.config['ACCOUNT_NAME']   # Azure account name
+key = app.config['ACCOUNT_KEY']      # Azure Storage account access key  
+connect_str = app.config['CONNECTION_STRING']
+container = app.config['CONTAINER'] # Container name
+allowed_ext = app.config['ALLOWED_EXTENSIONS'] # List of accepted extensions
+max_length = app.config['MAX_CONTENT_LENGTH'] # Maximum size of the uploaded file
 
-"""
-blob_service_client = BlobServiceClient.from_connection_string(conn_str=connect_str) # create a blob service client to interact with the storage account
-try:
-    container_client = blob_service_client.get_container_client(container=container_name) # get container client to interact with the container in which images will be stored
-    container_client.get_container_properties() # get properties of the container to force exception to be thrown if container does not exist
-except Exception as e:
-    print(e)
-    #print("Creating container...")
-    #container_client = blob_service_client.create_container(container_name) # create a container in the storage account if it does not exist
-"""
-@app.route('/')
-def index():
-    return render_template('index.html')
-    
-    
-@app.route('/page2')
-def page2():
-    from script.test import test
-    test()
-    #y = str(os.getcwd())
-    return "Executed"  
-    
-@app.route('/page3')  
-def page3():
-    #from script.test import test
-    #test()
-    #y = str(os.getcwd())
-    return render_template('page3.html') 
-"""
-@app.route('/')
-def index():
-    return "Hello World!"
-"""
-   
+blob_service_client = BlobServiceClient.from_connection_string(connect_str)
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in allowed_ext
+
+@app.route('/upload',methods=['POST'])
+def upload():
+    if request.method == 'POST':
+        img = request.files['file']
+        if img and allowed_file(img.filename):
+            filename = secure_filename(img.filename)
+            img.save(filename)
+            blob_client = blob_service_client.get_blob_client(container = container, blob = filename)
+            with open(filename, "rb") as data:
+                try:
+                    blob_client.upload_blob(data, overwrite=True)
+                    msg = "Upload Done ! "
+                except:
+                    pass
+            os.remove(filename)
+    return render_template("index.html", msg=msg)
 
 if __name__ == "__main__":
-    app.secret_key = "ThisisNotASecret:p"
-    app.run(debug=True)
+    app.run()
