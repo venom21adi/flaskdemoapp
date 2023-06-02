9ea748df5e5714835582a9a2309a28fa8ccac1a5
venom21adi <69112627+venom21adi@users.noreply.github.com>
2023-06-02 12:06:47 +0530

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-ACCOUNT_NAME = "flaskdemoappstorage"
-ACCOUNT_KEY = "prbvCQU0nU+u3/Svm8rL/+r3HZWUlSkzu7T5xHg8ihe3U08wxL+eQTC8PVIvf/AmQMHFbiJCufmA+AStoGmxGw=="
-CONNECTION_STRING = "DefaultEndpointsProtocol=https;AccountName=flaskdemoappstorage;AccountKey=prbvCQU0nU+u3/Svm8rL/+r3HZWUlSkzu7T5xHg8ihe3U08wxL+eQTC8PVIvf/AmQMHFbiJCufmA+AStoGmxGw==;EndpointSuffix=core.windows.net"
-CONTAINER = "Test"
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg'])
-MAX_CONTENT_LENGTH = 20 * 1024 * 1024    # 20 Mb limit
